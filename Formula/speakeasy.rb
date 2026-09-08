06137df94cf813621cc9a5b0a9ca14d0bfb106bf
jimfleming <jimmy.fleming@gmail.com>
2026-09-08 14:45:11 -0700

    Pin the formula to the v0.2.3 revision

--- Formula/speakeasy.rb
+++ Formula/speakeasy.rb
@@ -4,8 +4,8 @@ class Speakeasy < Formula
   desc "Speak Claude Code / Codex turns aloud via local Kokoro TTS"
   homepage "https://github.com/jimfleming/speakeasy"
   url "https://github.com/jimfleming/speakeasy.git",
-      tag:      "v0.2.2",
-      revision: "6f0796ebca439cdb8ab6cfdd9bcc7e29d99d7f14"
+      tag:      "v0.2.3",
+      revision: "52ff65052fd9cac9791044eb4233e680c531d6b0"
   license "Apache-2.0"
 
   depends_on arch: :arm64
